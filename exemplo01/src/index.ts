0bfea865f9eabd36d2f58d1b3d150d6c0c4937db
Sidney Sousa <sidney.sousa@ifms.edu.br>
2023-08-16 22:29:49 -0400

    finalizando exemplo01

--- exemplo01/src/index.ts
+++ exemplo01/src/index.ts
@@ -6,4 +6,6 @@ const client1 = new Client('999.999.999-99', 'Fulano de Tal')
 
 // Instanciação de um novo objeto da classe BankAccount
 const account1 = new BankAccount('1234', '0', client1)
+account1.branch = '123-4' // acessa o setter de _branch
+console.log(account1.branch) // acessa o getter de _branch
 console.log(account1)
--- exemplo01/src/models/BankAccount.ts
+++ exemplo01/src/models/BankAccount.ts
@@ -2,15 +2,84 @@ import { Client } from './Client'
 
 export class BankAccount {
   private _number: string
-  branch: string
+  private _branch: string
   private _balance: number
   private _client: Client
 
   constructor(number: string, branch: string, client: Client) {
-    this._number = number
-    this.branch = branch
+    this._number = this._validateNumber(number) ? number : '00000-0'
+    this._branch = this._validateBranch(branch) ? branch : '000-0'
     this._balance = 0
     this._client = client
+  }
+
+  /**
+   * Cria um novo objeto de conta bancária
+   * @param branch Número da agência
+   * @param client Cliente dono da conta
+   * @param number Número da conta
+   */
+  // constructor(branch: string, client: Client, number?: string) {
+  //   if (number && this._validateNumber(number)) {
+  //     this.number = number
+  //   }
+  //   this.number = '00000-0'
+  //   this.branch = this._validateBranch(branch) ? branch : '000-0'
+  //   this._balance = 0
+  //   this._client = client
+  // }
+
+  get branch() {
+    return this._branch
+  }
+
+  set branch(newValue: string) {
+    // const regex = /^\d{3}-\d{1}$/
+    // if(regex.test(newValue)) {
+
+    // }
+    /************************************/
+    // if(/^\d{3}-\d{1}$/.test(newValue)) {
+
+    // }
+    /************************************/
+    // if (newValue.match(/^\d{3}-\d{1}$/)) {
+    //   this._branch = newValue
+    // } else {
+    //   console.log('Invalid branch value')
+    // }
+    if (this._validateBranch(newValue)) {
+      this._branch = newValue
+    } else {
+      console.log('Invalid branch value')
+    }
+  }
+
+  /**
+   * Valida se o número da agência corresponde ao padrão 000-0.
+   * @param branch Número de agência a ser validado
+   * @returns true caso o número seja válido; false c.c.
+   */
+  private _validateBranch(branch: string): boolean {
+    const regex = /^\d{3}-\d{1}$/
+    return regex.test(branch)
+  }
+
+  get number() {
+    return this._number
+  }
+
+  set number(newValue: string) {
+    if (this._validateNumber(newValue)) {
+      this._number = newValue
+    } else {
+      console.log('Invalid number value')
+    }
+  }
+
+  private _validateNumber(number: string) {
+    const regex = /^\d{5}-\d{1}$/
+    return regex.test(number)
   }
 
   deposit(value: number) {
@@ -22,7 +91,7 @@ export class BankAccount {
     }
   }
 
-  withdraw(value: number) {
+  withdraw(value: number): void {
     if (value > 0 && value <= this._balance) {
       //this._balance = this._balance - value
       this._balance -= value
@@ -31,7 +100,7 @@ export class BankAccount {
     }
   }
 
-  statement() {
+  statement(): string {
     const formatter = Intl.NumberFormat('pt-BR', {
       style: 'currency',
       currency: 'brl',
